743e8bcf9b4772e069d58950132cf500dcfef99b
Jacqueline Arsenault <justcordelya@gmail.com>
2017-07-05 21:54:45 -0500

    Big Changes, Similar Function
    Form input changed to files instead of copy/paste.
    Attempted to use str_replace() or preg_replace() to replace user IDs in chat text with user names. Has not worked yet. broken code commented out.

--- index.php
+++ index.php
@@ -26,31 +26,60 @@ type, user (keyed to "id" in users.json), text, ts
 <body>
   <h1>Tension</h1>
 <h2>Slack Archives to HTML Converter</h2>
-<form id="chat" action="slack-logs.php" method="post">
-  <label for="users">Paste contents of users file: </label>
-<textarea name="users" id="users"></textarea>
+<form id="chat" action="index.php" enctype="multipart/form-data" method="post">
+  <label for="users">Select the users file: </label>
+  <input type="file" name="users.json" id="users"></input>
+<!--<textarea name="users" id="users"></textarea>-->
 <br/>
-<label for "chat">Paste contents of chat log file: </label>
-<textarea name="chat" id="chat"></textarea>
+<label for "chat">Select a chat log file: </label>
+<input type="file" name="chat.json" id="chat"></input>
+<!--<textarea name="chat" id="chat"></textarea>-->
 <input type="submit" value="Submit"/>
 </form>
 
 <?php
 
-if ($_POST) {
-  $chat = json_decode($_POST["chat"], true);
-  $users = json_decode($_POST["users"], true);
-  foreach($chat as $row) { // loop through each chat log record and replace user id with username
+if ($_FILES) {
+
+$users = json_decode(file_get_contents($_FILES['users_json']['tmp_name']), true);
+
+$chat = file_get_contents($_FILES['chat_json']['tmp_name']);
+
+$users_bracketed = $users;
+
+for ($i = 0; $i < count($users_bracketed); $i++) {
+$id = $users_bracketed[$i]['id'];
+$tmp = '<';
+$tmp .= $id;
+$tmp .= '>';
+$users_bracketed[$i]['id'] = $tmp;
+}
+/* This stuff does not work
+foreach ($users as $user) {
+str_replace($user['id'], $user['name'], $chat);
+
+}
+*/
+
+$chat = json_decode($chat, true);
+
+    foreach($chat as $row) { // loop through each chat log record and replace user id with username
     echo "<p><b>"; // open a paragraph and prepare to display username
-    $id = $row["user"];
+
+    $id = $row['user'];
         $name = null;
 
-    foreach ($users as $user) {
-      if ($user["id"] == $id) {
-        $name = $user["name"];
-        break;
-        }
-      }
+        foreach ($users as $user) {
+              if ($user["id"] == $id) {
+                $name = $user["name"];
+                break;
+                }
+              }
+
+
+
+
+
 
     echo $name;
     echo":</b><br/>";
@@ -60,10 +89,11 @@ if ($_POST) {
     echo date('m/d/y g:i:s a', $row["ts"]);
     echo "</small></p>";
     echo "<hr/>";
-    }
+  }
+}
 
-      }
-    
+
+
 
 
 
